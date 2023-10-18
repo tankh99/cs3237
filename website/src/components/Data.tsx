9132ed392755f3ed1c122ea1f095b0e7c1b914ce
tankh99 <khanghou99@gmail.com>
2023-10-18 15:20:30 +0800

    website: integrate frontend and server

---
+++ website/src/components/Data.tsx
@@ -0,0 +1,23 @@
+'use client'
+
+import React, { useEffect } from 'react'
+import { Button } from './ui/button'
+
+export default function Data() {
+  
+  const sendData = async () => {
+    try {
+      const res = await fetch(process.env.BACKEND_URL || 'http://localhost:4000')
+      const data = await res.json();
+      alert("Message successfully sent: " + data);
+    } catch (err: any) {
+      alert("Exception occurred " + err)
+    }
+
+  }
+  return (
+    <div>
+      <Button onClick={sendData}>Send message</Button>
+    </div>
+  )
+}

bd961d5f56e0b2ea5b9f691bb9e5c4ea9e9c9bd4
tankh99 <khanghou99@gmail.com>
2023-10-18 15:09:56 +0800

    init server: connect to iothub

---
+++ server/src/app.controller.ts
@@ -0,0 +1,38 @@
+import { Controller, Get } from '@nestjs/common';
+import { AppService } from './app.service';
+import { Registry, Client } from 'azure-iothub';
+
+@Controller()
+export class AppController {
+  constructor(private readonly appService: AppService) {}
+
+  @Get()
+  getHello(): string {
+    const connectionString = `HostName=CS3237-IOTHub.azure-devices.net;SharedAccessKeyName=iothubowner;SharedAccessKey=Jxwq/Ejdwlkx3nZXEd9lQdTrXIl7R3ldnAIoTNYJNjM=`;
+    const deviceId = 'ESP32-KH';
+    // eslint-disable-next-line @typescript-eslint/no-unused-vars
+    const registry = Registry.fromConnectionString(connectionString);
+    const client = Client.fromConnectionString(connectionString);
+
+    client.open((err) => {
+      if (err) {
+        console.error('Could not connect: ' + err.message);
+      } else {
+        console.log('Client connected');
+
+        client.on('message', (message) => {
+          console.log('Received message from device: ' + message.getData());
+        });
+      }
+    });
+
+    client.send(deviceId, 'Hello from the server', (err) => {
+      if (err) {
+        console.error('Error sending message:', err.toString());
+      } else {
+        console.log('Message sent successfully.');
+      }
+    });
+    return this.appService.getHello();
+  }
+}

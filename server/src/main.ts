5bce6d26c97ebe6ac56c61d9ee01fa1c1cc1c04d
tankh99 <khanghou99@gmail.com>
2023-10-18 15:19:32 +0800

    server: added CORS

--- server/src/main.ts
+++ server/src/main.ts
@@ -3,6 +3,10 @@ import { AppModule } from './app.module';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+  app.enableCors({
+    credentials: false,
+    origin: ['http://localhost:3000'],
+  });
   await app.listen(process.env.PORT || 4000);
 }
 bootstrap();

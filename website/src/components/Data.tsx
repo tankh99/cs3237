8bb9c1c8798712a3bbfa44d71ebabdf6512b282f
tankh99 <khanghou99@gmail.com>
2023-10-28 06:06:23 +0800

    feat: enable websocket on frontend

--- website/src/components/Data.tsx
+++ website/src/components/Data.tsx
@@ -2,22 +2,49 @@
 
 import React, { useEffect } from 'react'
 import { Button } from './ui/button'
+import useEvents, { IOTEvent } from '@/utils/hooks/useEvents';
+import { socket } from '@/utils/socket';
 
 export default function Data() {
-  
+  const [events] = useEvents();
   const sendData = async () => {
     try {
-      const res = await fetch(process.env.BACKEND_URL || 'http://localhost:4000')
+      const res = await fetch(process.env.NEXT_PUBLIC_API_URL || 'http://localhost:4000')
+      console.log(res)
       const data = await res.json();
       alert("Message successfully sent: " + data);
     } catch (err: any) {
       alert("Exception occurred " + err)
     }
   }
+
+  // const ping = () => {
+  //   console.log("Pinging", socket)
+  //   socket!.emit("events", {message: "Hey, ping pong"});
+  // }
+  const addEvent = () => {
+    console.log("Pinging", socket)
+    const event: IOTEvent = {
+      imu: {
+        x: Math.random(),
+        y: Math.random(),
+        z: Math.random()
+      }
+    }
+    socket!.emit("events", event);
+  }
   
   return (
     <div>
       <Button onClick={sendData}>Send message</Button>
+      {/* <Button onClick={ping}>Ping</Button> */}
+      <Button onClick={addEvent}>Add event</Button>
+      {events && events.map((event, index) => {
+        return (
+          <div key={index}>{index}</div>
+        )
+      })}
+
     </div>
   )
 }

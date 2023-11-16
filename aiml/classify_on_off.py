bc6845b3343aab10b2cac9171c8dd188f6e829fb
tankh99 <khanghou99@gmail.com>
2023-11-16 11:36:22 +0800

    feat: update tremor classification model

--- aiml/classify_on_off.py
+++ aiml/classify_on_off.py
@@ -9,26 +9,36 @@ import pandas as pd
 class Grouped2DCNN(nn.Module):
     def __init__(self):
         super(Grouped2DCNN, self).__init__()
-        self.conv1 = nn.Conv2d(in_channels=32, out_channels=32, kernel_size=(6, 1))
+        self.conv1 = nn.Conv2d(in_channels=3, out_channels=32, kernel_size=(5, 1), stride=(2, 1))
+        self.conv2 = nn.Conv2d(in_channels=32, out_channels=64, kernel_size=(5, 1), stride=(2, 1))
+        self.conv3 = nn.Conv2d(in_channels=64, out_channels=32, kernel_size=(5, 1), stride=(2, 1))
         self.maxpool1 = nn.MaxPool2d(kernel_size=(5, 1))
-        self.fc1 = nn.Linear(in_features=6, out_features=10)
-        self.dropout1 = nn.Dropout(0.1)
-        self.fc2 = nn.Linear(in_features=10, out_features=2)
+        self.dropout1 = nn.Dropout(0.6)
+        self.fc1 = nn.Linear(in_features=1472, out_features=32)
+        self.dropout2 = nn.Dropout(0.8)
+        self.fc2 = nn.Linear(in_features=32, out_features=2)
     def forward(self, x):
-        # torch.Size([32, 15, 3])
+#         print(x.shape) # torch.Size([32, 3, 1875, 1])
         x = self.conv1(x)
         x = F.relu(x)
-        # torch.Size([32, 10, 3])
+#         print(x.shape) # torch.Size([32, 32, 936, 1])
+        x = self.conv2(x)
+        x = F.relu(x)
+#         print(x.shape) # torch.Size([32, 64, 466, 1])
+        x = self.conv3(x)
+        x = F.relu(x)
+#         print(x.shape) # torch.Size([32, 32, 231, 1])
         x = self.maxpool1(x)
-        # torch.Size([32, 2, 3])
+        x = self.dropout1(x)
+#         print(x.shape) # torch.Size([32, 32, 46, 1])
         x = torch.flatten(x, 1)
-        # torch.Size([32, 6])
+#         print(x.shape) # torch.Size([32, 1472])
         x = self.fc1(x)
         x = F.relu(x)
-        x = self.dropout1(x)
-        # torch.Size([32, 10])
+        x = self.dropout2(x)
+#         print(x.shape) # torch.Size([32, 32])
         output = self.fc2(x)
-        # torch.Size([32, 2])
+#         print(output.shape) # torch.Size([32, 2])
         return output
 
 lr = 0.001
@@ -36,7 +46,7 @@ betas = (0.9, 0.999)
 model = Grouped2DCNN()
 optimizer = optim.NAdam(model.parameters(), lr=lr, betas=betas)
 
-file_path = './models/best_2dcnn_imu_onoff_model.pth'
+file_path = './models/BESTfinal_G180_window_noval_3conv_6_8DO_2stride_2dcnn_imu_onoff.pth'
 
 checkpoint = torch.load(file_path)
 model.load_state_dict(checkpoint['state_dict'])
